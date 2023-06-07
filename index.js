d56e7bf989dee4b066ed3f9846b2ad8e83d073a9
mdshamoon <mohdshamoon841@gmail.com>
2023-06-07 18:03:51 +0530

    fixes

--- index.js
+++ index.js
@@ -4,71 +4,52 @@ import axios from "axios";
 import express from "express";
 dotenv.config();
 import bodyParser from "body-parser";
-
 const app = express();
 const PORT = process.env.PORT || 3000;
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
 });
-
-app.post("/chat", async (req: any, res: any) => {
+app.post("/chat", async (req, res) => {
     const user_input = req.body.user_input;
     res(getAnswerFromJugalbandi(user_input));
 });
-
 const client = new DiscordJS.Client({
     intents: ["Guilds", "GuildMessages", GatewayIntentBits.MessageContent],
 });
-
-client.on("ready", async () => {});
-
+client.on("ready", async () => { });
 client.on("threadCreate", async (thread) => {
-    if (
-        thread.parent?.type === ChannelType.GuildForum &&
-        thread.parentId === process.env.CHANNEL_ID
-    ) {
+    if (thread.parent?.type === ChannelType.GuildForum &&
+        thread.parentId === process.env.CHANNEL_ID) {
         const firstMessage = await thread.fetchStarterMessage();
         const message = firstMessage?.content || "";
-
         const answer = await getAnswerFromJugalbandi(message);
-        const role = thread.guild.roles.cache.find(
-            (role) => role.name === "Glific Support"
-        );
+        const role = thread.guild.roles.cache.find((role) => role.name === "Glific Support");
         thread.send(answer);
-        thread.send(
-            role?.toString() +
-                " team please check if this needs any further attention."
-        );
+        thread.send(role?.toString() +
+            " team please check if this needs any further attention.");
     }
 });
-
 client.login(process.env.BOT_TOKEN);
-
-const getAnswerFromJugalbandi = async (message: string) => {
+const getAnswerFromJugalbandi = async (message) => {
     const apiUrl = process.env.API_URL;
-
     const params = {
         uuid_number: process.env.GLIFIC_DOC_UUID || "",
         query_string: encodeURIComponent(message),
     };
     // Convert the params object into a query string
     const queryParams = new URLSearchParams(params).toString();
-
     try {
         const result = await axios.get(`${apiUrl}?${queryParams}`, {
             timeout: 120000,
         });
         return result.data.answer;
-    } catch (e) {
+    }
+    catch (e) {
         return "Sorry, I am not able to answer this question due to timeout in API. Please try again later.";
     }
 };
-
-client.on("ready", async () => {});
-
+client.on("ready", async () => { });
 client.login(process.env.BOT_TOKEN);
