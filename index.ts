d78489e1b02be44a00985e4a05e9e3f813666c56
mdshamoon <mohdshamoon841@gmail.com>
2023-06-07 15:16:18 +0530

    added code for ai

--- index.ts
+++ index.ts
@@ -1,124 +1,72 @@
-import DiscordJS, { EmbedBuilder } from "discord.js";
 import dotenv from "dotenv";
-import express from "express";
-const { BigQuery } = require("@google-cloud/bigquery");
-var cron = require("node-cron");
-
+import DiscordJS, {
+    ChannelType,
+    GatewayIntentBits,
+  
+} from "discord.js";
+import axios from "axios";
+const express = require("express");
 dotenv.config();
+const bodyParser = require("body-parser");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
-
-app.get("/", (req, res) => {
-    res.send("Github issues bot!");
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
 });
 
-const client = new DiscordJS.Client({
-    intents: ["Guilds", "GuildMessages"],
+app.post("/chat", async (req: any, res: any) => {
+    const user_input = req.body.user_input;
+    res(getAnswerFromJugalbandi(user_input));
 });
 
-const sendMessagetoDiscord = async () => {
-    const bigquery = new BigQuery({
-        projectId: process.env.PROJECT_ID,
-        keyFilename: "./google-credentials.json",
-    });
-    // Queries the U.S. given names dataset for the state of Texas.
+const client = new DiscordJS.Client({
+    intents: ["Guilds", "GuildMessages", GatewayIntentBits.MessageContent],
+});
 
-    const query =
-        "SELECT * FROM `tides-saas-309509.917302307943.stats_all` where period = 'day' and inserted_at > current_date() LIMIT 100";
+client.on("ready", async () => {});
 
-    // For all options, see https://cloud.google.com/bigquery/docs/reference/rest/v2/jobs/query
-    const options = {
-        query: query,
-        // Location must match that of the dataset(s) referenced in the query.
-        location: "US",
-    };
+client.on("threadCreate", async (thread) => {
+    if (thread.parent?.type === ChannelType.GuildForum) {
+        const firstMessage = await thread.fetchStarterMessage();
+        const message = firstMessage?.content || "";
 
-    // Run the query as a job
-    const [job] = await bigquery.createQueryJob(options);
-    console.log(`Job ${job.id} started.`);
-
-    // Wait for the query to finish
-    const [rows] = await job.getQueryResults();
-
-    // Print the results
-
-    let totalContacts = 0;
-    let totalMessages = 0;
-
-    let organizationNames = "";
-    let messages = "";
-    let incomingVsOutgoingMessages = "";
-
-    rows.sort((a: any, b: any) => b.messages - a.messages);
-
-    rows.forEach((row: any) => {
-        totalContacts = totalContacts + row.contacts;
-        totalMessages = totalMessages + row.messages;
-
-        if (row.messages > 0) {
-            incomingVsOutgoingMessages =
-                incomingVsOutgoingMessages +
-                row.inbound +
-                " / " +
-                row.outbound +
-                "\n";
-
-            messages = messages + row.messages + "\n";
-            organizationNames =
-                organizationNames + row.organization_name + "\n";
-        }
-    });
-
-    const guildId = process.env.GUILD_ID || "";
-    const channleId = process.env.CHANNEL_ID || "";
-
-    const guild = client.guilds.cache.get(guildId);
-
-    let channels;
-
-    if (guild) {
-        channels = guild.channels;
-    } else {
-        channels = client.channels;
+        const answer = await getAnswerFromJugalbandi(message);
+        const role = thread.guild.roles.cache.find(
+            (role) => role.name === "Glific Support"
+        );
+        thread.send(answer);
+        thread.send(
+            role?.toString() +
+                " team please check if this needs any further attention."
+        );
     }
+});
 
-    const finalMessage = new EmbedBuilder()
-        .setColor("#0099ff")
-        .setTitle("Date: " + new Date().toLocaleDateString())
-        .setDescription(
-            "\n\n**Total contacts:** " +
-                totalContacts +
-                "\n**Messages yesterday:** " +
-                totalMessages
-        )
-        .addFields([
-            {
-                name: "NGO name",
-                value: organizationNames,
-                inline: true,
-            },
-            {
-                name: "Messages",
-                value: messages,
-                inline: true,
-            },
-            {
-                name: "Incoming / outgoing",
-                value: incomingVsOutgoingMessages,
-                inline: true,
-            },
-        ]);
+client.login(process.env.BOT_TOKEN);
 
-    const channel = channels.cache.get(channleId);
-    if (channel?.isText()) {
-        channel.send({ embeds: [finalMessage] });
+const getAnswerFromJugalbandi = async (message: string) => {
+    const apiUrl = process.env.API_URL;
+
+    const params = {
+        uuid_number: process.env.GLIFIC_DOC_UUID || "",
+        query_string: message,
+    };
+    // Convert the params object into a query string
+    const queryParams = new URLSearchParams(params).toString();
+
+    try {
+        const result = await axios.get(`${apiUrl}?${queryParams}`, {
+            timeout: 60000,
+        });
+        return result.data.answer;
+    } catch (e) {
+        return "Sorry, I am not able to answer this question due to timeout in API. Please try again later.";
     }
 };
 
@@ -126,6 +74,3 @@ client.on("ready", async () => {});
 
 client.login(process.env.BOT_TOKEN);
 
-cron.schedule("30 3 * * *", () => {
-    sendMessagetoDiscord();
-});
